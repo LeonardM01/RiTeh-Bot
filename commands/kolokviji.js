507e6a6689e946e289bfefd7ca91d65a599cdd95
Leonard Martinis <martinisleo@gmail.com>
2022-03-11 11:12:21 +0100

    Updating the message replies

--- commands/kolokviji.js
+++ commands/kolokviji.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'kolokviji',
     description:"Raspored kolokvija u semestru",
     execute(message, args){
-        message.channel.send('http://www.riteh.uniri.hr/media/filer_public/c1/01/c101d2c7-9519-415c-b8b4-e01c0389a685/plan_kolokvija_pss_ra_2021-2022.pdf');
+        message.reply('http://www.riteh.uniri.hr/media/filer_public/c1/01/c101d2c7-9519-415c-b8b4-e01c0389a685/plan_kolokvija_pss_ra_2021-2022.pdf');
     }
 }

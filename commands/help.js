380b41651de574cb35d63e95fa9dd808380fccab
LeonardM <martinisleo@gmail.com>
2022-03-12 11:10:20 +0100

    Revert "Merge branch 'master' into RiTeh-Bot/master"
    This reverts commit 3b2760b8b405398a350a3789573a6edc4284c75c.

--- commands/help.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'help',
-    description: "Help komanda za ispisivanje svi mogućih komandi",
-    execute(message,args){
-        message.reply("!ispiti - raspored završnih ispita\n!kolokviji - raspored kolokvija\n!raspored - link za raspored tekućeg semestra\n!news - news feed sa riteg.uniri.hr stranice\n!stup - poll za stavljanje nekoga na stup srama");
-    }
-}
--- commands/news.js
+++ commands/news.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'news',
     description: "Komanda za dobivanje novosti sa riteh.uniri.hr stranice",
     execute(message, args){
-        message.reply("News feed jos nije implementiran...hvala na čekanju!");
+        message.reply();
     }
 }

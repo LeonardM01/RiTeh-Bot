a067afc2b83a3ea73111576d253b6e91f4225486
LeonardM <martinisleo@gmail.com>
2022-03-12 13:56:56 +0100

    Making a automatic help command

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,14 @@
+const { DiscordAPIError, MessageEmbed } = require("discord.js")
+const fs = require("fs");
+
 module.exports = {
     name: 'help',
     description: "Help komanda za ispisivanje svi mogućih komandi",
     execute(message,args){
-        message.reply("!ispiti - raspored završnih ispita\n!kolokviji - raspored kolokvija\n!raspored - link za raspored tekućeg semestra\n!news - news feed sa riteg.uniri.hr stranice\n!stup - poll za stavljanje nekoga na stup srama");
+        const embed = new MessageEmbed()
+            .setTitle("Postojeće komande")
+            .setDescription()
+        
+        message.channel.send({embeds : [embed]});
     }
 }

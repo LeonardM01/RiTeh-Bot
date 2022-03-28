25758f0ad77ec6e2ca718cea1093a3fd5e1aba88
Leonard Martinis <86668031+LeonardM01@users.noreply.github.com>
2022-03-28 14:12:05 +0200

    Update poll.js

--- commands/util/poll.js
+++ commands/util/poll.js
@@ -1,11 +1,17 @@
 // poll import iz packagea
 const { poll } = require('discord.js-poll');
+const config = require('../../src/main').config; // databaza sa potrebnim podacima
 
 module.exports = {
     name: 'poll',
     description: 'Komanda za otvaranje glasovanja u odredenom kanalu',
     usage: '?poll [ime kanala] [ime glasovanja] + [opcije]+ [opcije]',
     execute(message, args) {
-        poll(message, args, '+', 'BLUE');
+        if(message.member._roles.find(role => role === config['admin-role']) || message.member.id === config.owner){
+            poll(message, args, '+', 'BLUE');
+        }else{
+            message.delete();
+            message.member.send("Niste moderator stoga, ne mozete napraviti poll :confused:");
+        }
     }
 }

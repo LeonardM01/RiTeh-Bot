6f47670fade2a21fd1024664dbfa73ad2757d696
LeonardM <martinisleo@gmail.com>
2022-04-06 22:03:20 +0200

    Rewritten ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -12,29 +12,34 @@ module.exports = {
         args.shift();
         let reason = args.join(" ");
         // embed poruka koja se salje
-        const embed = new MessageEmbed()
-            .setThumbnail(member.displayAvatarURL({dynamic: true}))
-            .setColor('BLUE')
-            .setTitle(member.nickname + " je bannan:exclamation:")
-            .setDescription(message.member.nickname + " je bannao usera: " + member.nickname + "\nRazlog: " + reason)
-            .setTimestamp()
-        //funkcija koja checka ako autor poruke ima administrator ulogu
-        if(message.member._roles.find(role => role === cache.moderator) || message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)){
-            // ban function
-            member.ban({reason: reason});
-            // tekstualni kanal u koji se salju poruke o moderaciji
-            client.channels.cache.get(cache.log).send({embeds : [embed]});
-            // notify the user
-            try{
-                if(reason.length != 0){
-                    await member.send(reason);
+        if(member != undefined){
+            const embed = new MessageEmbed()
+                .setThumbnail(member.displayAvatarURL({dynamic: true}))
+                .setColor('BLUE')
+                .setTitle(member.nickname + " je bannan:exclamation:")
+                .setDescription(message.member.nickname + " je bannao usera: " + member.nickname + "\nRazlog: " + reason)
+                .setTimestamp()
+        
+            //funkcija koja checka ako autor poruke ima administrator ulogu
+            if(message.member._roles.find(role => role === cache.moderator) || message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)){
+                // ban function
+                member.ban({reason: reason});
+                // tekstualni kanal u koji se salju poruke o moderaciji
+                client.channels.cache.get(cache.log).send({embeds : [embed]});
+                // notify the user
+                try{
+                    if(reason.length != 0){
+                        await member.send(reason);
+                    }
+                }catch(error){
+                    console.error(error);
                 }
-            }catch(error){
-                console.error(error);
+            // ako member nije admin
+            }else{
+                message.reply("Niste administrator!");
             }
-        // ako member nije admin
         }else{
-            message.reply("Niste administrator!");
+            message.reply("Niste @-ali membera!");
         }
     }
 }

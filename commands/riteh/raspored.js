6f126ba2052e483a68eba7baa7179981b170dd7b
LeonardM01 <martinisleo@gmail.com>
2022-09-07 12:58:21 +0200

    Added different schedule options

--- commands/riteh/raspored.js
+++ commands/riteh/raspored.js
@@ -3,13 +3,43 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('raspored')
-    .setDescription('Komanda za prikazivanje rasporeda predavanja tekuće akademske godine'),
+    .setDescription('Komanda za prikazivanje rasporeda predavanja tekuće akademske godine')
+    .addStringOption(option =>
+      option.setName('semestar')
+        .setDescription('Izaberite tekući semestar')
+        .setRequired(true)
+        .addChoices(
+          { name: 'Prvi', value: '1' },
+          { name: 'Treći', value: '3' },
+          { name: 'Peti', value: '5' },
+        ),
+    ),
   async execute(interaction) {
-    await interaction.reply({
-      embeds: [new EmbedBuilder().setColor('BLUE')
-        .setImage('https://i.imgur.com/ibDBYdP.png')
-        .setTitle('Raspored semestra')
-        .setURL('http://www.riteh.uniri.hr/media/filer_public/43/5a/435a8f75-142a-4fdd-9b8c-6612721d9e30/raspored_pss_rac_2022_2023_zimski_v1.pdf')],
-    });
+    switch (interaction.options.getString('semestar')) {
+      case '1':
+        await interaction.reply({
+          embeds: [new EmbedBuilder().setColor('BLUE')
+            .setImage('https://i.imgur.com/hy73BIR.png')
+            .setTitle(`Raspored ${interaction.options.getString('semestar')}. semestra`)
+            .setURL('https://imgur.com/a/Uww0MPn')],
+        });
+        break;
+      case '3':
+        await interaction.reply({
+          embeds: [new EmbedBuilder().setColor('BLUE')
+            .setImage('https://i.imgur.com/ldLLIP5.png')
+            .setTitle(`Raspored ${interaction.options.getString('semestar')}. semestra`)
+            .setURL('https://imgur.com/a/99NYAPG')],
+        });
+        break;
+      case '5':
+        await interaction.reply({
+          embeds: [new EmbedBuilder().setColor('BLUE')
+            .setImage('https://i.imgur.com/78JuNQc.png')
+            .setTitle(`Raspored ${interaction.options.getString('semestar')}. semestra`)
+            .setURL('https://imgur.com/a/OniHXUt')],
+        });
+        break;
+    }
   },
 };
